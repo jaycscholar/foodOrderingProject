b05fb493db5e9047f815cdbfc0f50f62270ea705
Jay Barrios <reyezsj@gmail.com>
2025-10-17 05:01:45 -0700

    added sample menu items

---
+++ leftColumn.js
@@ -0,0 +1,73 @@
+const leftColumn = document.getElementById("left-column");
+               console.log(leftColumn)
+
+               leftColumn.innerHTML =
+                    `
+
+<button id="show menu button" class="bg-green-200 p-2 rounded block lg:hidden w-full" onclick="hideNavItems()">
+                    Show options ---
+               </button>  
+  
+               <nav id="navigation" class="bg-yellow-200 p-2 rounded lg:col-span-1">
+                    <button class="w-full p-2 bg-white rounded mb-2 text-left"> Create An Account </button>
+                    <button class="w-full p-2 bg-white rounded mb-2 text-left"> Login </button>
+                    <button class="w-full p-2 bg-white rounded mb-2 text-left hidden" id="logoutB"> Logout </button>
+                    <button class="w-full p-2 bg-white rounded mb-2 text-left"> View My Tray <div id="trayQ"
+                              class="inline">(x)</div></button>
+                    <button class="w-full p-2 bg-white rounded mb-2 text-left"> Track My order</button>
+                    <button class="w-full p-2 bg-white rounded mb-2 text-left"> About</button>
+               </nav>
+
+`
+
+                    ;
+
+               //showLogOut(); 
+
+
+               function showLogOut() {
+                    const button = document.getElementById('logoutB')
+                    let toHide = button.className
+
+                    let toShow = toHide.replace('hidden', '');
+                    button.className = toShow
+
+               }
+
+let showingNav = false;   
+
+
+hideNavItems();
+ 
+
+
+               function hideNavItems () {
+
+                    const navigation = document.getElementById('navigation');
+ 
+                    if (showingNav === false) {  
+
+                    let toShow = navigation.className;
+                    let toHide = toShow + ' hidden lg:block' 
+                    navigation.className = toHide 
+console.log("items Hidden");
+                    showingNav = true;
+ 
+                    } else if (showingNav === true) {
+
+                    let toShow = navigation.className;
+                    let toHide = toShow.replace(' hidden lg:block', '');
+                    navigation.className = toHide 
+console.log("items shown");
+                    showingNav = false;
+                         
+                    }
+
+
+               } 
+
+
+
+
+
+               
---
+++ menu.js
@@ -0,0 +1,29 @@
+ addMenu ();
+
+     function addMenu (){
+     const theMenu = document.getElementById('menuArea')
+     theMenu.innerHTML = `
+<h2 id="Menu Title" class="lg:col-span-3 text-xl"> The Menu</h2>  
+     `
+ 
+let menuItems = ['fry', 'coin', 'sock', 'pie','rock', 'guitar','star']
+
+menuItems.forEach((fruit) => {
+const menuItem = document.createElement("div");
+menuItem.className = `col-span-3 lg:col-span-1 h-52 p-3 bg-blue-100` 
+menuItem.innerHTML = ` ${fruit} 
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+Ut enim ad minim veniam, quis nostrud exercitation 
+<button>Add Item </button>
+`
+
+     theMenu.appendChild(menuItem)  
+})
+
+
+
+
+     }
+
