e10a41ef72da78201ff8c1011ff462d57c9c07a3
Jay Barrios <reyezsj@gmail.com>
2025-10-20 17:34:58 -0700

    working on menu and added two more items

--- menu.js
+++ menu.js
@@ -1,30 +1,80 @@
- addMenu ();
+let menuItems = []
 
-     function addMenu (){
-     const theMenu = document.getElementById('menuArea')
-     theMenu.innerHTML = `
-<h2 id="Menu Title" class="lg:col-span-3 text-xl"> The Menu</h2>  
-     `
- 
-let menuItems = ['fry', 'coin', 'sock', 'pie','rock', 'guitar','star']
 
-menuItems.forEach((food) => {
-const menuItem = document.createElement("div");
-menuItem.className = `col-span-3 lg:col-span-1 h-52 p-3 bg-blue-100` 
-menuItem.innerHTML = ` ${food} 
+addMenu();
+
+function addMenu() {
+
+
+fetchAndDisplayMenu();
+
+
+     menuItems.forEach((food) => {
+          const menuItem = document.createElement("div");
+          menuItem.className = `col-span-3 lg:col-span-1 h-52 p-3 bg-blue-100`
+          menuItem.innerHTML = ` ${food} 
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
 sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
 Ut enim ad minim veniam, quis nostrud exercitation 
-<input type="number" min="1" max="3" placeholder="1" class="inline" id='${food}' ></input>
-<button class= "border border-color-red px-2 inline" >Add  </button> 
-
+<form> 
+<input type="number" min="1" max="9" placeholder="1" class="inline" id='${food}number' ></input>
+<button type='button' class= "border border-color-red px-2 inline" >Add  </button> 
+</form>
 `
 
-     theMenu.appendChild(menuItem)  
-})
+          theMenu.appendChild(menuItem)
+     })
 
 
+
+}
+
+async function fetchAndDisplayMenu() {
+
+     const theMenu = document.getElementById('menuArea')
+     theMenu.innerHTML = `
+     <h2 id="Menu Title" class="lg:col-span-3 text-3xl"> The Menu</h2>  
+     `
+
+          try {
+               const response = await fetch("fullMenu.json")
+               const menu = await response.json();
+
+               menuItems.push(...menu)
+
+               console.log(menuItems)
+
+     menuItems.forEach((food) => {
+           const menuItem = document.createElement("div");  
+                     menuItem.className = `col-span-3 lg:col-span-1 h-60 p-3 bg-blue-100`
+         
+                     menuItem.id = food['name']
+                     menuItem.innerHTML = ` 
+                     <h3 class="text-2xl">
+                     ${food['name']} 
+                     </h3>
+
+                     <p class="h-38 overflow-y-auto"> 
+                     ${food['description']}    
+                     </p>                  
+                     
+<form> 
+<input type="number" min="1" max="9" placeholder="1" class="inline" id='${food}number' ></input>
+<button type='button' class= "border border-color-red px-2 inline" >Add</button> 
+</form>
+
+                     `  
+          
+                    theMenu.appendChild(menuItem)
+     
+     })
+
+
+          } catch (error) {
+               alert("Error fetching user data with async/await:", error);
+          }
 
      }
+
 
